dc9bbfe0dbf834a0adb298de1f99fc2b20f21808
知了软件 <zlsoftdq@sina.com>
2016-12-23 16:58:52 +0800

    module

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 /**
  * gulp的主文件，用于注册任务
- * @Author: iceStone
- * @Date:   2016-01-26 17:07:26
- * @Last Modified by:   iceStone
- * @Last Modified time: 2016-01-26 17:56:34
+ * @Author: zlsoftdq
+ * @Date:   2016-12-20 17:07:26
+ * @Last Modified by:   zlsoftdq
+ * @Last Modified time: 2016-12-23 17:56:34
  */
 
 'use strict';
@@ -11,6 +11,7 @@
 // 此处代码都是由NODE执行
 // 载入Gulp模块
 var gulp = require('gulp');
+var uglify= require('gulp-uglify');
 //var less = require('gulp-less');
 
 // 注册一个任务
@@ -34,11 +35,17 @@ gulp.task('dist', function() {
 
 var cssnano = require('gulp-cssnano');
 
-gulp.task('style', function() {
+gulp.task('compress_css', function() {
   gulp.src('src/*.css')
     //.pipe(less()) // 该环节过后就是CSS
     .pipe(cssnano())
     .pipe(gulp.dest('css/'));
+});
+gulp.task('compress_js', function() {
+  gulp.src('src/*.js')
+    //.pipe(less()) // 该环节过后就是CSS
+    .pipe(uglify())
+    .pipe(gulp.dest('js/'));
 });
 /*
 var browserSync = require('browser-sync').create();
